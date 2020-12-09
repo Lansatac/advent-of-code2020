16a8e6e822347e0cf3cdaef06f8736d7e0e541e9
Jason Yegge <Lansatac@gmail.com>
2020-12-09 09:08:24 -0700

    day 2 part 1

--- day2/day2a.d
+++ day2/day2a.d
@@ -3,6 +3,7 @@ import std.algorithm;
 import std.ascii;
 import std.array;
 import std.typecons;
+import std.conv;
 
 void main()
 {
@@ -10,15 +11,26 @@ void main()
         File("input.txt")
         .byLine(KeepTerminator.no, newline)
         .map!(pw=>pw.split(": "))
-        .map!(pw=>tuple!("rules", "password")(
-            pw[0].split(" "),
-            pw[1]
-            ))
-        .map!(x);
+        .map!(pw=>
+            {
+                auto password = pw[1];
+                auto rulePart = pw[0].split(" ");
+                auto minMax = rulePart[0].split("-");
+                auto min = minMax[0].to!int;
+                auto max = minMax[1].to!int;
+                auto letter = rulePart[1];
+                auto letterCount = password.count(letter);
+                return tuple!("rules", "password", "matching")(
+                    tuple!("min", "max", "letter")(min, max, letter),
+                    password,
+                    letterCount >= min && letterCount <= max);
+            }
+        )//[("1-2 a", "abc", 1)]
+        .map!(x=>x()) // Can't figure out how to disambiguate the lambda above :(
+        .count!((x, y) => x.matching == y)(true)
+            ;
 
-    foreach (pw; passwords)
-    {
-        writeln(pw);
-    }
+    writeln(passwords);
+
 }
 
